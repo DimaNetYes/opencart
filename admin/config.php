f5a87007ddc3ca36569072560b57edf22d76cd28
Dima <realwindrunner@gmail.com>
2018-02-06 13:35:36 +0200

    starting to add image sale

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocs/admin/');
+define('HTTP_CATALOG', 'http://ocs/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocs/admin/');
+define('HTTPS_CATALOG', 'http://ocs/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\OSPanel\domains\ocs/admin/');
+define('DIR_SYSTEM', 'D:\OSPanel\domains\ocs/system/');
+define('DIR_DATABASE', 'D:\OSPanel\domains\ocs/system/database/');
+define('DIR_LANGUAGE', 'D:\OSPanel\domains\ocs/admin/language/');
+define('DIR_TEMPLATE', 'D:\OSPanel\domains\ocs/admin/view/template/');
+define('DIR_CONFIG', 'D:\OSPanel\domains\ocs/system/config/');
+define('DIR_IMAGE', 'D:\OSPanel\domains\ocs/image/');
+define('DIR_CACHE', 'D:\OSPanel\domains\ocs/system/cache/');
+define('DIR_DOWNLOAD', 'D:\OSPanel\domains\ocs/download/');
+define('DIR_LOGS', 'D:\OSPanel\domains\ocs/system/logs/');
+define('DIR_CATALOG', 'D:\OSPanel\domains\ocs/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqliz');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ocshop');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocs/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocs/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\OSPanel\domains\ocs/catalog/');
+define('DIR_SYSTEM', 'D:\OSPanel\domains\ocs/system/');
+define('DIR_DATABASE', 'D:\OSPanel\domains\ocs/system/database/');
+define('DIR_LANGUAGE', 'D:\OSPanel\domains\ocs/catalog/language/');
+define('DIR_TEMPLATE', 'D:\OSPanel\domains\ocs/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\OSPanel\domains\ocs/system/config/');
+define('DIR_IMAGE', 'D:\OSPanel\domains\ocs/image/');
+define('DIR_CACHE', 'D:\OSPanel\domains\ocs/system/cache/');
+define('DIR_DOWNLOAD', 'D:\OSPanel\domains\ocs/download/');
+define('DIR_LOGS', 'D:\OSPanel\domains\ocs/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqliz');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ocshop');
+define('DB_PREFIX', 'oc_');
+?>
